660075ee163e74e6016a2be1315e13b49bcf0bfb
Rachel Williams <rachelwilliams659@gmail.com>
2020-05-10 07:56:43 -0600

    Add first draft of project iterations 0, 1, 2 & 3

--- src/book.js
+++ src/book.js
@@ -1,12 +1,40 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+return {name: name, age: age, pronouns: pronouns};
+};
+
+function saveReview(singleReview, reviews) {
+  if (reviews.includes(singleReview) === false) {
+  reviews.push(singleReview);
 }
+};
+
+function calculatePageCount(title) {
+  return 20*(title.length);
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+};
+
+function editBook(book) {
+  return Math.round(book.pageCount *= 3/4);
+};
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
--- src/librarian.js
+++ src/librarian.js
@@ -1,8 +1,40 @@
-class Librarian {
-  constructor() {
-
+ class Librarian {
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
-  
-}
+
+  greetPatron(patron, morning) {
+    if (morning) {
+      return `Good morning, ${patron}!`
+    } else {
+    return `Hello, ${patron}!`
+    }
+  }
+//Access the library value (object) for this class instance
+//Access shelves value;
+//Search thru each shelf, looking at each object in shelf array
+//If book matches object.title, return `Yes we have ${book}`'
+// var arrayOfShelves = Object.values(this.library.shelves);
+// if (arrayOfShelves.some(shelf => shelf.some(book => book.title === title))) {
+  findBook(title) {
+    var allShelves = this.library.shelves;
+    for (var key in allShelves) {
+      var bookIndex = allShelves[key].findIndex(book => book.title === title);
+      if (bookIndex >= 0) {
+        allShelves[key].splice(bookIndex, 1);
+        return `Yes, we have ${title}`;
+      } else {
+      return `Sorry, we do not have ${title}`;
+      }
+    }
+  }
+
+  calculateLateFee(days) {
+    return Math.ceil(days * .25)
+  };
+
+};
+
 
 module.exports = Librarian;
